b2afcd66cb8b52b2300f2d9c990de6bf36b24ae1
dobrybajer <kamil.zakk@gmail.com>
2017-02-25 23:43:28 +0100

    Initial commit - empty project

---
+++ GolfGame/GolfGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GolfGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
